5ffe366e99c3abf0fb85a0a0222fcbd054401c76
tuananhdao <kitsudo1412@gmail.com>
2026-06-27 16:22:13 +0200

    .

--- data/DoNotStarveTogether/Cluster_1/Caves/worldgenoverride.lua
+++ data/DoNotStarveTogether/Cluster_1/Caves/worldgenoverride.lua
@@ -1,5 +1,7 @@
 return {
   override_enabled = true,
   preset = "DST_CAVE",
+  overrides = {
+    world_size = "small",
+  },
 }
-
---
+++ data/DoNotStarveTogether/Cluster_1/Master/worldgenoverride.lua
@@ -0,0 +1,12 @@
+return {
+  override_enabled = true,
+  preset = "SURVIVAL_TOGETHER",
+  overrides = {
+    berrybush = "often",
+    basicresource_regrowth = "always",
+    grass = "often",
+    regrowth = "fast",
+    sapling = "often",
+    world_size = "huge",
+  },
+}
